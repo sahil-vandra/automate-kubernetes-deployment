3fe0ca66d0b2e8fcbe2f42c57ff2b0b1b0be0fc6
sahil-vandra <98399594+sahil-vandra@users.noreply.github.com>
2022-02-22 19:03:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Sahil Vandra" }');
+    res.send('{ "response": "Hello From Sahil Vandra today is 22 Feb" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello World from 22 Feb" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works! " }');
+    res.send('{ "response": " Great!, It works! on 22 Feb" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
